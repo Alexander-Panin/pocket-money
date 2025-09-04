c0e6d82457fe76020f4524d5d42ebd2183385e23
Your Name <you@example.com>
2025-09-04 12:21:46 +0200

    using render in rust

--- wasm/src/store.rs
+++ wasm/src/store.rs
@@ -3,6 +3,13 @@ use web_sys::window;
 pub fn store() -> Option<u32> {
     let local_storage = window()?.local_storage().ok()??;
     let x = local_storage.length();
-    let _ = local_storage.set_item(&format!("{:?}", x), "foobar");
+    let s = render()?;
+    let _ = local_storage.set_item(&format!("{:?} {}", x, s), "foobar");
     x.ok()
+}
+
+pub fn render() -> Option<String> {
+    let document = window()?.document()?;
+    let x = document.query_selector(&"#one").ok()??;
+    Some(x.id())
 }
