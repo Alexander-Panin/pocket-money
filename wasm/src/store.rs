1117a254737a44b28690557e1c660523d3867025
Your Name <you@example.com>
2025-09-05 14:16:35 +0200

    a bit nicer render in rust

--- wasm/src/store.rs
+++ wasm/src/store.rs
@@ -1,5 +1,5 @@
 use wasm_bindgen::prelude::*;
-use web_sys::{window, HtmlTemplateElement};
+use web_sys::{window, HtmlTemplateElement, DocumentFragment};
 
 pub fn store() -> Option<u32> {
     let local_storage = window()?.local_storage().ok()??;
@@ -19,20 +19,22 @@ pub fn render_template(main: &str, t: &str) {
     let _ = render(main, t);
 }
 
-fn render(main:& str, t: &str) -> Option<String> {
+fn render(main:& str, t: &str) -> Option<()> {
     let document = window()?.document()?;
     let main = document.query_selector(main).ok()??;
     let template = document.query_selector(t).ok()??.dyn_into::<HtmlTemplateElement>().ok()?;
 
     for x in 1..5 {
-        let clone = template.content().clone_node_with_deep(true).ok()?;
-        let nodes = clone.child_nodes();
-        alert(&format!("{}", nodes.length()));
-        nodes.item(1)?.set_text_content(Some(&format!("{x}")));
-        nodes.item(3)?.set_text_content(Some(&format!("{x}+{x}+{x}")));
-        let _ = main.append_child(&clone);
+        let root = template
+            .content()
+            .clone_node_with_deep(true).ok()?
+            .dyn_into::<DocumentFragment>().ok()?;
+        let nodes = root.query_selector_all("div").ok()?;
+        nodes.item(0)?.set_text_content(Some(&format!("{x}")));
+        nodes.item(1)?.set_text_content(Some(&format!("{x}+{x}+{x}")));
+        let _ = main.append_child(&root);
     }
-    Some("".to_owned())
+    Some(())
 }
 
 
