9fadff3b66ae049937920336bc0ed5707f89222f
Your Name <you@example.com>
2025-09-08 09:20:56 +0200

    rust log errors

--- ts/src/listen.ts
+++ ts/src/listen.ts
@@ -1,3 +1,50 @@
-export function foo(a: number, b: number): number {
-	return a+b;
+let __popup_handler = (ev: Event) => {};
+
+export const handler = (popupId: string) => (event: Event) => {
+	const {__id: rowId = null, __action: action} = get_attrs(event.target as Element);
+	switch (action) {
+		case 'list/row':
+			link(popupId, popup_handler(rowId));
+			show(popupId);
+			return;
+		case 'popup/close':
+			unlink(popupId);
+			hide(popupId);
+	}
+}
+
+function unlink(popupId: string) {
+	document
+		.querySelector(popupId)
+	   ?.removeEventListener('click', __popup_handler);
+}
+
+function link(popupId: string, handler: (e: Event) => void) {
+	const x = document.querySelector(popupId);
+	x?.removeEventListener('click', __popup_handler);
+	__popup_handler = handler;
+	x?.addEventListener('click', __popup_handler);
+}
+
+const show = (popupId: string) => 
+	{ (document.querySelector(popupId) as HTMLElement).hidden = false; }
+
+const hide = (popupId: string) => 
+	{ (document.querySelector(popupId) as HTMLElement).hidden = true; }
+
+const popup_handler = (rowId: string | null) => (event: Event) => {
+	const {__action: action} = get_attrs(event.target as Element);
+	switch (action) {
+		case 'popup/update': 
+			event.stopPropagation();
+			console.log("popup/update XXXX, rowId:", rowId);
+	}
+}
+
+function get_attrs(elem: Element) {
+	const map = {} as Record<string, string>;
+	for (let x of elem.attributes) {
+		map[x.name] = x.value;
+	}
+	return map;
 }
--- wasm/src/store.rs
+++ wasm/src/store.rs
@@ -1,5 +1,11 @@
 use wasm_bindgen::prelude::*;
-use web_sys::{window, HtmlTemplateElement, DocumentFragment};
+use web_sys::{
+    window, 
+    HtmlTemplateElement, 
+    DocumentFragment, 
+    Element, 
+    console,
+};
 
 pub fn store() -> Option<u32> {
     let local_storage = window()?.local_storage().ok()??;
@@ -9,26 +15,67 @@ pub fn store() -> Option<u32> {
 }
 
 #[wasm_bindgen]
-pub fn render_template(main: &str, t: &str) {
-    let _ = render(main, t);
+pub fn view(list_id: &str, row_id: &str, popup_id: &str) {
+    let r = render(list_id, row_id, popup_id);
+    let _ = r.map_err(|x| console::error_1(&x));
 }
 
-fn render(main:& str, t: &str) -> Option<()> {
-    let document = window()?.document()?;
-    let main = document.query_selector(main).ok()??;
-    let template = document.query_selector(t).ok()??.dyn_into::<HtmlTemplateElement>().ok()?;
+fn render(list_id: &str, row_id: &str, popup_id: &str) -> Result<(), JsValue> {
+    let document = window()
+        .ok_or(JsValue::from_str("can not find window"))?
+        .document()
+        .ok_or(JsValue::from_str("can not find document"))?;
+        
+    let list = document
+        .query_selector(&format!("#{list_id}"))?
+        .ok_or(JsValue::from_str("can not find list"))?;
+    let list_template = document
+        .query_selector(&format!("#template-{list_id}"))?
+        .ok_or(JsValue::from_str("can not find template-list"))?
+        .dyn_into::<HtmlTemplateElement>()?;
+    let content = list_template
+        .content()
+        .clone_node_with_deep(true)?
+        .dyn_into::<DocumentFragment>()?;
+    let _ = list.append_child(&content);
 
+    let row = document
+        .query_selector(&format!("#{row_id}"))?
+        .ok_or(JsValue::from_str("can not find row"))?;
+    let row_template = document
+        .query_selector(&format!("#template-{row_id}"))?
+        .ok_or(JsValue::from_str("can not find template-row"))?
+        .dyn_into::<HtmlTemplateElement>()?;
     for x in 1..5 {
-        let root = template
+        let content = row_template
             .content()
-            .clone_node_with_deep(true).ok()?
-            .dyn_into::<DocumentFragment>().ok()?;
-        let nodes = root.query_selector_all("div").ok()?;
-        nodes.item(0)?.set_text_content(Some(&format!("{x}")));
-        nodes.item(1)?.set_text_content(Some(&format!("{x}+{x}+{x}")));
-        let _ = main.append_child(&root);
+            .clone_node_with_deep(true)?
+            .dyn_into::<DocumentFragment>()?;
+        let nodes = content.query_selector_all("div")?;
+        let node = nodes
+            .item(0)
+            .ok_or(JsValue::from_str("no nodes[0] in row-template"))?;
+        node.set_text_content(Some("hello world!"));
+        let _ = node
+            .dyn_into::<Element>()?
+            .set_attribute("__id", &format!("{x}"));
+        let _ = row.append_child(&content);
     }
-    Some(())
+
+    let popup = document
+        .query_selector(&format!("#{popup_id}"))?
+        .ok_or(JsValue::from_str("can not find popup"))?;
+    let popup_template = document
+        .query_selector(&format!("#template-{popup_id}"))?
+        .ok_or(JsValue::from_str("can not find template-popup"))?
+        .dyn_into::<HtmlTemplateElement>()?;
+    let content = popup_template
+        .content()
+        .clone_node_with_deep(true)?
+        .dyn_into::<DocumentFragment>()?;
+    let _ = popup.append_child(&content);
+
+    Ok(())
 }
 
 
