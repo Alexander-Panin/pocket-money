5ddc80246dc4444d3cf9ea83feb51d75948db9a6
Your Name <you@example.com>
2025-09-11 18:24:34 +0200

    css by hover

--- ts/src/listen.ts
+++ ts/src/listen.ts
@@ -1,7 +1,16 @@
 let __popup_handler = (ev: Event) => {};
 
+function target(node: Element | null): Element | null {
+	while (node?.attributes && !get_attrs(node.attributes).__action) { 
+		node = node.parentNode as Element; 
+	} 
+	return node;
+} 
+
 export const handler = (popupId: string) => (event: Event) => {
-	const {__id: rowId = null, __action: action} = get_attrs(event.target as Element);
+	const node = target(event.target as Element);
+	console.assert(Boolean(node?.attributes), "not found node");
+	const {__id: rowId = null, __action: action} = get_attrs(node!.attributes);
 	switch (action) {
 		case 'list/row':
 			link(popupId, popup_handler(rowId));
@@ -33,7 +42,7 @@ const hide = (popupId: string) =>
 	{ (document.querySelector(popupId) as HTMLElement).hidden = true; }
 
 const popup_handler = (rowId: string | null) => (event: Event) => {
-	const {__action: action} = get_attrs(event.target as Element);
+	const {__action: action} = get_attrs((event.target as Element).attributes);
 	switch (action) {
 		case 'popup/update': 
 			event.stopPropagation();
@@ -41,9 +50,9 @@ const popup_handler = (rowId: string | null) => (event: Event) => {
 	}
 }
 
-function get_attrs(elem: Element) {
+function get_attrs(attributes: NamedNodeMap) {
 	const map = {} as Record<string, string>;
-	for (let x of elem.attributes) {
+	for (let x of attributes) {
 		map[x.name] = x.value;
 	}
 	return map;
