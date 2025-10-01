035204d1eb666d4c18cf055066863fbe681873f5
Your Name <you@example.com>
2025-10-01 13:48:12 +0200

    adding web server

---
+++ server/src/main.rs
@@ -0,0 +1,19 @@
+use actix_web::{get, web, App, HttpServer, Responder};
+use actix_files::Files;
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| { 
+        App::new()
+            .service(Files::new("/static", "../static").prefer_utf8(true))
+            .service(greet)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
