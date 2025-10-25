5f0a2e22a690fa0dad6ca7122741b6d8677e53f7
Your Name <you@example.com>
2025-10-26 01:25:42 +0200

    forgotten files

---
+++ ts/src/common/utils.ts
@@ -0,0 +1,9 @@
+export function getMonths(locale: string = 'en'): string[] {
+  return locale === 'ru'
+    ? ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']
+    : ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+}
+
+export function getMonthBy(month: string, locale: string): string {
+  return getMonths(locale)[getMonths().indexOf(month)] ?? "not found"; 
+}
