9571b14168cb10ac1290f6a0fecf0657485bc237
Your Name <you@example.com>
2025-12-20 09:28:52 +0100

    adding 2026

--- ts/src/calendar/view.ts
+++ ts/src/calendar/view.ts
@@ -1,8 +1,12 @@
 import getWasm from "../common/wasm";
 import { getMonthBy } from "../common/utils";
 
+const MONTHS_2025 = ['august', 'september', 'october', 'november', 'december'];
+const MONTHS_2026 = ['january', 'february', 'march'];
+
 export class View {
-	constructor() {}
+	tmpNodes: Array<Element>
+	constructor() { this.tmpNodes = []; }
 
 	prerender() {
 		document
@@ -10,26 +14,47 @@ export class View {
 			.appendChild(
 				(document.querySelector("#template-list") as HTMLTemplateElement).content
 			);
+		this.tmpNodes.push(
+			...this.list(2025, MONTHS_2025.map((month) => ([month, 0]))),
+			...this.list(2026, MONTHS_2026.map((month) => ([month, 0])))
+		);
 	}
 
+	async money(year: number, months: string[]): Promise<Array<[string, number]>> {
+		const result = [];
+		for (const month of months) { 
+      		const sum = await getWasm().Store.sum(`${year}:${month}`);
+      		const pair: [string, number] = [month, sum];
+			result.push(pair);
+		}
+		return result;
+	}	
+	
 	async render() {
-		const months = ['august', 'september', 'october', 'november', 'december'];
-		const container = document.querySelector("#container-row")!;
-		for (const month of months) {
-			// TODO 2025
-      		const sum = await getWasm().Store.sum(`2025:${month}`);
-      		const row = (document.querySelector("#template-row") as HTMLTemplateElement).content;
-      		container.appendChild(this.fill(row.cloneNode(true) as HTMLElement, sum, month));
-		};
+		const data = await this.money(2025, MONTHS_2025); 
+		const data2 = await this.money(2026, MONTHS_2026); 
+		this.tmpNodes.forEach(x => x.remove());
+		this.list(2025, data);
+		this.list(2026, data2);
 	}
 
-	fill(x: HTMLElement, sum: number, month: string) {
+	list(year: number, data: Array<[string, number]>) {
+		const container = document.querySelector(`#list-row-${year}`)!;
+		const result = [];
+		for (const [month, sum] of data) {
+      		const row = (document.querySelector("#template-row") as HTMLTemplateElement).content;
+      		container.appendChild(this.fill(row.cloneNode(true) as HTMLElement, sum, year, month));
+      		result.push(container.lastElementChild!);
+		};
+		return result;
+	}
+
+	fill(x: HTMLElement, sum: number, year: number, month: string) {
   		x.querySelector('#row-sum')!.textContent = String(sum);
   		x.querySelector('#row-link')!.textContent = getMonthBy(month, 'ru');
-  		// TODO 2025
-  		(x.querySelector('#row-link') as HTMLAnchorElement)!.href = `/?month=${month}&year=2025`;
-  		x.querySelector('#row-download')!.setAttribute('__id', `2025:${month}`);
-  		x.querySelector('#row-download')!.setAttribute('download', `2025_${month}.csv`);
+  		(x.querySelector('#row-link') as HTMLAnchorElement)!.href = `/?month=${month}&year=${year}`;
+  		x.querySelector('#row-download')!.setAttribute('__id', `${year}:${month}`);
+  		x.querySelector('#row-download')!.setAttribute('download', `${year}_${month}.csv`);
   		return x;
 	}
 }
