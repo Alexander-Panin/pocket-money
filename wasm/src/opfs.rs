24a84e725027772b167bde94876b6ddf75bdd2de
Your Name <you@example.com>
2025-12-21 10:00:40 +0100

    rust: opfs in worker

--- wasm/src/opfs.rs
+++ wasm/src/opfs.rs
@@ -4,10 +4,17 @@ use web_sys::{
     FileSystemGetFileOptions, 
     FileSystemDirectoryHandle, 
     FileSystemFileHandle, 
+    FileSystemSyncAccessHandle,
     FileSystemWritableFileStream, 
     File, 
+    WorkerGlobalScope,
 };
-use web_sys::js_sys::{Promise, JsString};
+use web_sys::js_sys::{
+    Promise, 
+    JsString, 
+    global, 
+    Uint8Array
+};
 use wasm_bindgen_futures::{JsFuture};
 
 async fn future<T: JsCast>(p: Promise) -> Result<T, JsString> {
@@ -48,6 +55,34 @@ async fn file_handle(key: &str) -> Result<FileSystemFileHandle, JsString> {
     future(p).await
 }
 
+#[wasm_bindgen]
+pub async fn read_file_in_worker(id: JsString, name: JsString) -> Result<JsString, JsValue> {
+    let key = &(id.concat(&name)).as_string().unwrap_or_default();
+    let storage = global()
+        .unchecked_into::<WorkerGlobalScope>()
+        .navigator()
+        .storage();
+    let root: FileSystemDirectoryHandle = future(storage.get_directory()).await?;
+    let handle: FileSystemFileHandle = future(root.get_file_handle(key)).await?;
+    let file: File = future(handle.get_file()).await?;
+    Ok(JsFuture::from(file.text()).await?.into())
+}
 
-
-
+#[wasm_bindgen]
+pub async fn write_file_in_worker(id: JsString, name: JsString, value: JsString) -> Result<(), JsValue> {
+    let key = &(id.concat(&name)).as_string().unwrap_or_default();
+    let value = &value.as_string().unwrap_or_default();
+    let storage = global()
+        .unchecked_into::<WorkerGlobalScope>()
+        .navigator()
+        .storage();
+    let root: FileSystemDirectoryHandle = future(storage.get_directory()).await?;
+    let options = FileSystemGetFileOptions::new();
+    options.set_create(true);
+    let handle: FileSystemFileHandle = future(root.get_file_handle_with_options(&key, &options)).await?;
+    let sync_handle: FileSystemSyncAccessHandle = future(handle.create_sync_access_handle()).await?;
+    sync_handle.write_with_buffer_source(&Uint8Array::from(value.as_bytes()))?;
+    sync_handle.flush()?; 
+    sync_handle.close();
+    Ok(())
+}
