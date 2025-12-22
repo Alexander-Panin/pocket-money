3b4167ccab661c6649c115d0100a5c58b1fb753a
Your Name <you@example.com>
2025-12-22 19:45:08 +0100

    fix typos

--- static/worker.js
+++ static/worker.js
@@ -1,28 +1,28 @@
 import init, * as wasm from "/script/pkg/wasm.js?t=1763158241768";
 
-  self.onmessage = async (e) => {
+self.onmessage = async (e) => {
     await init();
     const { id: mid, type, msg } = e.data;
     const {id, value, ns, prevNs} = msg;
     const sendBack = x => self.postMessage({id: mid, msg: x});
     switch (type) {
-      case "append": 
-        wasm.Store.append(ns, id).then(sendBack); 
-        break;
-      case "repeat_regular": 
-        wasm.Store.repeat_regular(ns, prevNs).then(sendBack); 
-        break;
-      case "save_price": 
-        wasm.save_price(id, value).then(sendBack); 
-        break;
-      case "save_tag": 
-        wasm.save_tag(id, value).then(sendBack); 
-        break;
-      case "save_date": 
-        wasm.save_date(id, value).then(sendBack); 
-        break;
-      case "save_comment": 
-        wasm.save_comment(id, value).then(sendBack); 
-        break;
+        case "append": 
+            wasm.Store.append(ns, id).then(sendBack); 
+            break;
+        case "repeat_regular": 
+            wasm.Store.repeat_regular(ns, prevNs).then(sendBack); 
+            break;
+        case "save_price": 
+            wasm.save_price(id, value).then(sendBack); 
+            break;
+        case "save_tag": 
+            wasm.save_tag(id, value).then(sendBack); 
+            break;
+        case "save_date": 
+            wasm.save_date(id, value).then(sendBack); 
+            break;
+        case "save_comment": 
+            wasm.save_comment(id, value).then(sendBack); 
+            break;
     }
-  }
+}
